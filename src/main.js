343914026a5355c147b97d11db6db198879bc32a
anetrebsky <aleks.netrebskiy@gmail.com>
2024-04-23 15:13:45 +0400

    Add info

--- src/main.js
+++ src/main.js
@@ -10,10 +10,11 @@ async function run() {
     const ms = core.getInput('milliseconds', { required: true })
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.info(`Waiting ${ms} milliseconds ...`)
 
     // Log the current timestamp, wait, then log the new timestamp
     core.debug(new Date().toTimeString())
+    
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
 
